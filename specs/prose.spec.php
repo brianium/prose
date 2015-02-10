944143deaf6a1a80515e409df4502d51aa1dc5f7
Brian Scaturro <scaturrob@gmail.com>
2015-02-10 15:54:25 -0500

    initial commit

---
+++ specs/prose.spec.php
@@ -0,0 +1,22 @@
+<?php
+describe('Prose', function () {
+	describe('->preview()', function () {
+
+	});
+
+	describe('->publish()', function () {
+
+	});
+
+	describe('->summary()', function () {
+
+	});
+
+	describe('->coupons()', function () {
+
+	});
+
+	describe('->coupon()', function () {
+
+	});
+});
