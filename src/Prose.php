7f99eac556db89592e149cd809dc0917b0e01c8f
Brian Scaturro <scaturrob@gmail.com>
2015-02-10 16:41:47 -0500

    provide method, url and key via requester

--- src/Prose.php
+++ src/Prose.php
@@ -3,13 +3,15 @@ namespace Brianium\Prose;
 
 class Prose
 {
+    protected $requester;
+
 	public function setHttpRequester($requester)
 	{
-
+        $this->requester = $requester;
 	}
 
-	public function preview()
+	public function preview($slug)
 	{
-		
+        $this->requester->request('POST', "https://leanpub.com/$slug/preview.json", 'api_key=12345');
 	}
 }
