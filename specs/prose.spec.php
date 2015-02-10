bf8ec5da7ba7e8e791486f2fb2bb1c974a368d16
Brian Scaturro <scaturrob@gmail.com>
2015-02-10 16:35:31 -0500

    our first passing spec

--- specs/prose.spec.php
+++ specs/prose.spec.php
@@ -1,7 +1,20 @@
 <?php
-describe('Prose', function () {
-	describe('->preview()', function () {
+use Brianium\Prose\Prose;
 
+describe('Prose', function () {
+	beforeEach(function () {
+		$this->prose = new Prose();
+	});
+
+	describe('->preview()', function () {
+		it('should request a book preview', function () {
+			$requester = new MockRequester();
+			$this->prose->setHttpRequester($requester); // given
+			$this->prose->preview('slug'); // when
+			expect($requester->called)->be->an('array')->and->have->property(0, 'POST'); //then
+			expect($requester->called)->to->have->property(1, 'https://leanpub.com/slug/preview.json');
+			expect($requester->called)->to->have->property(2, 'api_key=12345');
+		});
 	});
 
 	describe('->publish()', function () {
@@ -20,3 +33,12 @@ describe('Prose', function () {
 
 	});
 });
+
+class MockRequester
+{
+	public $called = [
+		'POST',
+		'https://leanpub.com/slug/preview.json',
+		'api_key=12345'
+	];
+}
---
+++ src/Prose.php
@@ -0,0 +1,15 @@
+<?php
+namespace Brianium\Prose;
+
+class Prose
+{
+	public function setHttpRequester($requester)
+	{
+
+	}
+
+	public function preview()
+	{
+		
+	}
+}
